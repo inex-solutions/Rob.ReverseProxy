b4207cddb9c88374d320ddaead4f46014bda15bd
Rob Levine <rob@roblevine.co.uk>
2018-01-15 19:05:32 +0000

    Add service

---
+++ src/Rob.ReverseProxy.Service/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rob.ReverseProxy.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var service = new ReverseProxyService();
+
+            if (Environment.UserInteractive)
+            {
+                service.StartService(args);
+                Console.WriteLine("Service started");
+                Console.WriteLine("Press enter to exit");
+                service.StopService();
+                Console.ReadLine();
+            }
+            else
+            {
+                ServiceBase.Run(new[] {service});
+            }
+        }
+    }
+}
---
+++ src/Rob.ReverseProxy.Service/ReverseProxyService.cs
@@ -0,0 +1,32 @@
+﻿using System.ServiceProcess;
+
+namespace Rob.ReverseProxy.Service
+{
+    public partial class ReverseProxyService : ServiceBase
+    {
+        public ReverseProxyService()
+        {
+            InitializeComponent();
+        }
+
+        public void StartService(string[] args)
+        {
+
+        }
+
+        public void StopService()
+        {
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StartService(args);
+        }
+
+        protected override void OnStop()
+        {
+            StopService();
+        }
+    }
+}
