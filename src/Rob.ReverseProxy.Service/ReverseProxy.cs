ccef7108fcb087e7025fb7c3cdce17660c63610e
Rob Levine <rob@roblevine.co.uk>
2018-01-17 22:20:19 +0000

    switch copy strategy when chunked

--- src/Rob.ReverseProxy.Service/ReverseProxy.cs
+++ src/Rob.ReverseProxy.Service/ReverseProxy.cs
@@ -50,9 +50,9 @@ namespace Rob.ReverseProxy.Service
                 forwardingRequest.Headers.Host = host;
                 forwardingRequest.RequestUri = new Uri($"http://{host}{outerOwinContext.Request.Path}{outerOwinContext.Request.QueryString}");
 
-                var cancellationToken = new CancellationTokenSource(5000).Token;
-
-                var forwardingResponse = await forwardingClient.SendAsync(forwardingRequest, cancellationToken);
+                var cancellationTokenSource = new CancellationTokenSource(100000);
+                var cancellationToken = cancellationTokenSource.Token;
+                var forwardingResponse = await forwardingClient.SendAsync(forwardingRequest, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
                 outerOwinContext.Response.StatusCode = (int)forwardingResponse.StatusCode;
                 outerOwinContext.Response.ContentType = forwardingResponse.Content?.Headers.ContentType?.MediaType;
                 outerOwinContext.Response.ReasonPhrase = forwardingResponse.ReasonPhrase;
@@ -62,15 +62,32 @@ namespace Rob.ReverseProxy.Service
                     outerOwinContext.Response.Headers[header.Key] = header.Value.FirstOrDefault();
                 }
             
-                outerOwinContext.Response.Headers.Remove("transfer-encoding");
-
                 if (forwardingResponse.Content != null)
                 {
                     foreach (var contentHeader in forwardingResponse.Content.Headers)
                     {
                         outerOwinContext.Response.Headers[contentHeader.Key] = contentHeader.Value.FirstOrDefault();
                     }
-                    await forwardingResponse.Content.CopyToAsync(outerOwinContext.Response.Body);
+
+                    Stream streamToReadFrom = await forwardingResponse.Content.ReadAsStreamAsync();
+
+                    if (forwardingResponse.Headers.TransferEncodingChunked.HasValue
+                        && forwardingResponse.Headers.TransferEncodingChunked.Value)
+                    {
+                        int read;
+                        while ((read = streamToReadFrom.ReadByte()) != -1)
+                        {
+                            cancellationTokenSource.CancelAfter(100000);
+
+                            outerOwinContext.Response.Body.WriteByte((byte)read);
+                            //Console.Write(".");
+                        }
+                    }
+                    else
+                    {
+                        //Console.Write($"-{forwardingResponse.Content.Headers.ContentLength}");
+                        await streamToReadFrom.CopyToAsync(outerOwinContext.Response.Body);
+                    }
                 }
 
             }
